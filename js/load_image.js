ca6be9ffed474504d53c2c1302a0856abecbb8c4
Jess <jessbodie@gmail.com>
2018-04-02 18:41:19 -0400

    Add ESLint

--- js/load_image.js
+++ js/load_image.js
@@ -1,6 +1,6 @@
 // Load an image to a div after CSS, content loads
 var loadImage = (function(imageID) {
-  window.addEventListener("load", function(event) {
+  window.addEventListener("load", function() {
     var newImage = document.createElement("img");
     newImage.className = "obligatory__image-crazy";
     newImage.src = "img/profile_crazy_cropped.jpg";
